80c4834885acad57da2875971013e7ad3dfcde50
Steve Pentland <stevepentland@gmail.com>
2019-11-11 19:37:38 -0500

    Add clap for cli interaction

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate clap;
 
 mod config;
 mod error;
 mod system;
 
+use clap::{App, Arg};
 use config::read_in_config;
 use error::SpinupError;
 use system::extract_distro_details;
 
 fn main() -> Result<(), SpinupError> {
+    let matches = App::new("Spinup")
+        .version(crate_version!())
+        .author("Steve Pentland")
+        .about("Helps you spin up your new environment!")
+        .arg(Arg::with_name("verbose").short("-v"))
+        .get_matches();
     let details = extract_distro_details()?;
     let config = read_in_config("./data/sample.toml")?;
     println!("{:#?}", config);
