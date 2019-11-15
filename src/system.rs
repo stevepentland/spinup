42f706b7dcf427192b443605f4fbe22efe2441da
Steve Pentland <stevepentland@gmail.com>
2019-11-14 23:55:06 -0500

    Add paste crate for testing
    This allows concatenation of ident values when defining
    a macro. As the concat_ident macro in std is not likely
    to ever make it into stable, this is the best bet.
    
    Using this means that test function prefixes can be templated
    out and only the suffixes need be passed in.

--- src/system.rs
+++ src/system.rs
@@ -71,84 +71,72 @@ pub fn extract_distro_details() -> Result<SystemDetails, SpinupError> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    macro_rules! os_info {
-        ( $x:expr, $y:expr ) => {
-            sys_info::LinuxOSReleaseInfo {
-                id: $x,
-                id_like: $y,
-                name: None,
-                pretty_name: None,
-                version: None,
-                version_id: None,
-                version_codename: None,
-                ansi_color: None,
-                cpe_name: None,
-                build_id: None,
-                variant: None,
-                variant_id: None,
-                home_url: None,
-                bug_report_url: None,
-                support_url: None,
-                documentation_url: None,
-                logo: None,
-            };
-        };
-    }
-
+    use paste;
     macro_rules! os_val_test {
         ( $name:ident, $x:expr, $y:expr, $exp:expr ) => {
-            #[test]
-            fn $name() {
-                let info = os_info!($x, $y);
-                let sd = SystemDetails::from(info);
-                assert_eq!($exp, sd.target_os)
-            }
+            paste::item!(
+                #[test]
+                fn [<test_release_info_ $name>]() {
+                    let info = sys_info::LinuxOSReleaseInfo {
+                        id: $x,
+                        id_like: $y,
+                        name: None,
+                        pretty_name: None,
+                        version: None,
+                        version_id: None,
+                        version_codename: None,
+                        ansi_color: None,
+                        cpe_name: None,
+                        build_id: None,
+                        variant: None,
+                        variant_id: None,
+                        home_url: None,
+                        bug_report_url: None,
+                        support_url: None,
+                        documentation_url: None,
+                        logo: None,
+                    };
+                    let sd = SystemDetails::from(info);
+                    assert_eq!($exp, sd.target_os)
+                }
+            );
         };
         ( $name:ident, $x:expr, $exp:expr ) => {
-            #[test]
-            fn $name() {
-                let info = os_info!($x, None);
-                let sd = SystemDetails::from(info);
-                assert_eq!($exp, sd.target_os)
-            }
+            os_val_test!($name, $x, None, $exp);
         };
     }
 
     os_val_test!(
-        test_release_info_arch,
+        arch,
         Some(String::from("arch")),
         TargetOperatingSystem::Arch
     );
     os_val_test!(
-        test_release_info_manjaro,
+        manjaro,
         Some(String::from("manjaro")),
         TargetOperatingSystem::Arch
     );
     os_val_test!(
-        test_release_info_id_like_arch,
+        id_like_arch,
         None,
         Some(String::from("arch")),
         TargetOperatingSystem::Arch
     );
     os_val_test!(
-        test_release_info_id_like_manjaro,
+        id_like_manjaro,
         None,
         Some(String::from("manjaro")),
         TargetOperatingSystem::Arch
     );
+    os_val_test!(all_none, None, TargetOperatingSystem::Unknown);
     os_val_test!(
-        test_release_info_all_none,
-        None,
-        TargetOperatingSystem::Unknown
-    );
-    os_val_test!(
-        test_release_info_unknown_id_arch_id_like,
+        unknown_id_arch_id_like,
         Some(String::from("bsd")),
         Some(String::from("arch")),
         TargetOperatingSystem::Arch
     );
     os_val_test!(
-        test_release_info_both_unknown,
+        both_unknown,
         Some(String::from("bsd")),
         Some(String::from("mac")),
         TargetOperatingSystem::Unknown
