7693ab49899e09eb0d7418bc3e9dde3312f21589
Steve Pentland <stevepentland@gmail.com>
2019-11-23 14:54:52 -0500

    Add the ability to specify packages based on distro
    Since the names for packages may not be universal, this provides
    a way to denote packages that should only be installed on
    certain distros.

--- src/config.rs
+++ src/config.rs
@@ -4,19 +4,30 @@ use std::io::Read;
 
 use crate::error::SpinupError;
 
+// pub enum DistroPackages {
+//     Arch(Vec<String>),
+//     Debian(Vec<String>),
+//     Ubuntu(Vec<String>),
+// }
+
+#[derive(Debug, Deserialize)]
+pub struct DistroPackages {
+    pub target_os: String,
+    pub packages: Option<Vec<String>>,
+}
+
 #[derive(Debug, Deserialize)]
 pub struct Configuration {
     pub packages: Option<Vec<String>>,
+    pub distro_packages: Option<Vec<DistroPackages>>,
 }
 
 pub fn read_in_config(config_path: &str) -> Result<Configuration, SpinupError> {
     if let Ok(mut file) = File::open(config_path) {
         let mut contents = String::new();
         if file.read_to_string(&mut contents).is_ok() {
-            let config_r = toml::from_str::<Configuration>(&contents);
-            if let Ok(config) = config_r {
-                return Ok(config);
-            }
+            let config = toml::from_str::<Configuration>(&contents).unwrap();
+            return Ok(config);
         }
     }
     Err(SpinupError::ConfigurationReadError(String::from(
